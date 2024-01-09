bdd0fdbc4e04e82874daa172bdcf0b98bde9f6cb
Glitch (glitch-hello-website) <none>
2024-01-09 22:55:10 +0000

    🌴🏢 Checkpoint ./index.html:5221719/41191 ./script.js:5221719/1634

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function changeFont(fontName) {
   document.getElementsByTagName("body")[0].style.fontFamily = fontName;
 }
+
+function hideImages() {
+  for (let elem of document.getElementsByClassName("mnemonics")) {
+    elem.hidden = !elem.hidden;
+  }
+  
+  let button = document.getElementById('hideImagesButton')
+  if (button.innerHTML == 'Hide Images') {
+    button.innerHTML = 'Show Images'
+  } else {
+    button.innerHTML = 'Hide Images'
+  }
+}
